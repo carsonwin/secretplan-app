c01796b86de81a167047c15731726678d58aab47
Carson Winchester <111301781+carsonwin@users.noreply.github.com>
2024-03-16 19:31:17 -0400

    project tasks modal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 import node from '@astrojs/node'
 import 'dotenv/config'
